3037ee35b469fc6258cb67c22de32c68b3216a07
the-geek <itech@riseup.net>
2014-05-11 14:52:58 -0400

    Minor fix for plot 4

--- plot4.R
+++ plot4.R
@@ -21,7 +21,7 @@ axis(1,seq(0,nrow(data),length.out=3),c("Thu","Fri","Sat"))
 plot(data$Sub_metering_1,type='l',main="",xlab="",ylab="Energy sub metering",xaxt = "n")
 lines(data$Sub_metering_2,col='red')
 lines(data$Sub_metering_3,col='blue')
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),ncol=1,cex=1,lwd=1,bty='n')
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),ncol=1,cex=0.95,lwd=1,bty='n')
 axis(1,seq(0,nrow(data),length.out=3),c("Thu","Fri","Sat"))
 
 
